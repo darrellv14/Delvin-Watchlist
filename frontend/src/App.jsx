0227ab41966a050d6024eb59d3191d5785108eeb
github_darrellv14 <darrell.valentino14@gmail.com>
2025-04-14 02:44:02 +0700

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import "./css/App.css";
+import NavBar from './components/NavBar';
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { MovieProvider } from './context/MovieContext';
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import "../css/Navbar.css";
+
+export default function NavBar() {
+  return <nav className='navbar'>
+    <div className="navbar-brand">
+      <Link to="/">Delvin's Watchlist</Link>
+    </div>
+    <div className="navbar-links">
+      <Link to="/" className='nav-link'>Home</Link>
+      <Link to="/favorites" className='nav-link'>Favorites</Link>
+    </div>
+  </nav>
+}
